e5b085874cc69aceadd6ba47ab3c156768c93a7c
SmartW0rker <hristijanchochevski@gmail.com>
2025-03-10 17:24:26 +0100

    adding Eureka Server

--- employee-service/src/main/java/com/kiko/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/kiko/employeeservice/service/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "department-service", url = "http://localhost:9090")
+@FeignClient(name = "department-service")
 public interface APIClient {
     @GetMapping("api/departments/{department-code}")
     DepartmentDto getDepartment(@PathVariable("department-code") String departmentCode);
